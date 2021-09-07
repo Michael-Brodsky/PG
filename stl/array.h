b24025456ad2cbab818c8471470745024ee7d21c
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2021-09-07 01:30:11 -0600

    Update array.h

--- stl/array.h
+++ stl/array.h
@@ -6,7 +6,7 @@
  *
  *	File: array.h
  *	Date: August 22, 2021
- *	Version: 0.99
+ *	Version: 1.0
  *	Author: Michael Brodsky
  *	Email: mbrodskiis@gmail.com
  *	Copyright (c) 2012-2021 Michael Brodsky
