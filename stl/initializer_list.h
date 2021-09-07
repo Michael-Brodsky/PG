f8cd000ff17acb546b3b985575260d53dda0e5bd
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2021-09-07 01:40:16 -0600

    Update initializer_list.h

--- stl/initializer_list.h
+++ stl/initializer_list.h
@@ -6,7 +6,7 @@
  *
  *	File: initializer_list.h
  *	Date: August 30, 2021
- *	Version: 0.99
+ *	Version: 1.0
  *	Author: Michael Brodsky
  *	Email: mbrodskiis@gmail.com
  *	Copyright (c) 2012-2021 Michael Brodsky
