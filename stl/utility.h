f9ba432e7fe965ebb527448cf1fb820bca88a243
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2021-09-07 02:07:50 -0600

    Update utility.h

--- stl/utility.h
+++ stl/utility.h
@@ -6,7 +6,7 @@
  *
  *	File: utility.h
  *	Date: September 3, 2021
- *	Version: 0.99
+ *	Version: 1.0
  *	Author: Michael Brodsky
  *	Email: mbrodskiis@gmail.com
  *	Copyright (c) 2012-2021 Michael Brodsky
