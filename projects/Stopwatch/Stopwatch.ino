efc252f5e1e240ac11d855b3a303ccaff5eb22ff
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2021-10-03 12:18:43 -0600

    Add files via upload

--- projects/Stopwatch/Stopwatch.ino
+++ projects/Stopwatch/Stopwatch.ino
@@ -4,14 +4,18 @@
 #include <components/AnalogKeypad.h>
 #include <utilities/TaskScheduler.h>
 
+// Demonstration of a simple stopwatch program.
+// Detailed descriptions can be found in the comments of the #include files.
+
 using namespace pg;
 using namespace std::chrono;
-using Display = LCDDisplay<16, 2>;
-using Scheduler = pg::TaskScheduler<>;
-using Stopwatch = Timer<milliseconds>;
-using tenths = duration<std::time_t, std::deci>;
+using Display = LCDDisplay<16, 2>; // Type alias for a 16x2 lcd display.
+using Scheduler = pg::TaskScheduler<>; // Type alias for the default TaskScheduler (has millisecond resolution).
+using Stopwatch = Timer<milliseconds>;  // Type alias for millisecond timer.
+using tenths = duration<std::time_t, std::deci>; // Type alias for tenths of a second duration.
 
-enum class pg::ButtonTag 
+// Button tag enumerator, use descriptive names for each button you have wired up.
+enum class Keypad::Button::Tag 
 {
 	Right = 0,
 	Up,
@@ -20,38 +24,48 @@ enum class pg::ButtonTag
 	Select
 };
 
+// Forward decls.
 void refresh_display();
 void keypad_callback(const Keypad::Button* button, Keypad::Event event);
 
-const pin_t KeypadInputPin = 0;
-const analog_t LeftButtonTriggerLevel = 600;
-const analog_t SelectButtonTriggerLevel = 800;
-Keypad::Button btn_left(ButtonTag::Left, LeftButtonTriggerLevel);
-Keypad::Button btn_select(ButtonTag::Select, SelectButtonTriggerLevel);
-Keypad keypad({ &btn_left, &btn_select }, KeypadInputPin, &keypad_callback);
+// Keypad Button objects. Select: start/stop, Left: reset.
+const pin_t KeypadInputPin = 0; // Keypad input is AN0.
+const analog_t LeftButtonTriggerLevel = 600; // Button trigger levels depend on how buttons are wired to board.
+const analog_t SelectButtonTriggerLevel = 800;  // If using an LCD/Keypad Shield (recommended), use the published values.
+Keypad::Button btn_left(Keypad::Button::Tag::Left, LeftButtonTriggerLevel); // Resets the stopwatch.
+Keypad::Button btn_select(Keypad::Button::Tag::Select, SelectButtonTriggerLevel); // Starts/stops the stopwatch.
+Keypad keypad({ &btn_left, &btn_select }, KeypadInputPin, &keypad_callback); // Keypad params are { &buttons_list }, pin, &callback_func.
 
-Display::Field fld_hours = { 6,0,"","%02u:" };
+// Display Screen and Field objects. 16x2 Display is formatted thus (t is tenths): 
+//
+// *****************
+// *     hh:mm:ss.t* 
+// *               *
+// *****************
+
+Display::Field fld_hours = { 6,0,"","%02u:" }; // Field params are {col,row,"label","format"} (see printf() format specifiers).
 Display::Field fld_mins = { 9,0,"","%02u:" };
 Display::Field fld_secs = { 12,0,"","%02u." };
 Display::Field fld_tenths = { 15,0,"","%1u" };
-Display::Screen screen({ &fld_hours,&fld_mins,&fld_secs,&fld_tenths }, "");
+Display::Screen screen({ &fld_hours,&fld_mins,&fld_secs,&fld_tenths }, ""); // Screen params are { fields_list }, "label".
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-Display display(&lcd, &refresh_display, &screen);
+Display display(&lcd, &refresh_display, &screen); // Display params are &LiquidCrystal, &callback_func, &active_screen.
 
-ClockCommand clock_keypad(&keypad);
+// TaskScheduler & Task objects.
+ClockCommand clock_keypad(&keypad); // ClockCommand params are &clockable_object.
 ClockCommand clock_display(&display);
-Scheduler::Task task1(milliseconds(100), &clock_keypad, Scheduler::Task::State::Active);
+Scheduler::Task task1(milliseconds(100), &clock_keypad, Scheduler::Task::State::Active); // Task params are std::chrono::duration, &ClockCommand, Task::State
 Scheduler::Task task2(milliseconds(100), &clock_display, Scheduler::Task::State::Active);
 Scheduler scheduler({ &task1, &task2 });
 
-Stopwatch timer;
-bool running = false;
+Stopwatch timer; // The stopwatch timer that tracks elapsed time.
+bool running = false; // Flag indicating whether stopwatch is running.
 
 void setup() 
 {
-	lcd.begin(Display::cols(), Display::rows());
-	display.refresh(0, 0, 0, 0);
-	scheduler.start();
+	lcd.begin(Display::cols(), Display::rows()); // Params are Display template parameters. 
+	display.refresh(0, 0, 0, 0); // Displays 00:00:00.0 on the lcd.
+	scheduler.start(); // Don't forget to start the task scheduler or nothing will happen.
 }
 
 
@@ -62,6 +76,7 @@ void loop()
 
 void refresh_display()
 {
+  // extract hh, mm, ss & tenths from milliseconds.
 	milliseconds ms = timer.elapsed();
 	auto tenth = std::chrono::duration_cast<tenths>(ms) % 10;
 	ms -= std::chrono::duration_cast<milliseconds>(tenth);
@@ -72,9 +87,10 @@ void refresh_display()
 	auto hour = std::chrono::duration_cast<hours>(mins);
 	mins -= std::chrono::duration_cast<minutes>(hour);
 
-	display.refresh(hour, mins, secs, tenth);
+	display.refresh(hour, mins, secs, tenth); // Print it!
 }
 
+// Handles button press events. Usually ginormous, nested switch() statements, but here pretty simple.
 void keypad_callback(const Keypad::Button* button, Keypad::Event event)
 {
 	switch (event)
@@ -82,11 +98,11 @@ void keypad_callback(const Keypad::Button* button, Keypad::Event event)
 	case Keypad::Event::Press:
 		switch (button->tag_)
 		{
-		case ButtonTag::Select:
+		case Keypad::Button::Tag::Select: // Select button pressed.
 			if ((running = !running)) timer.resume();
 			else timer.stop();
 			break;
-		case ButtonTag::Left:
+		case Keypad::Button::Tag::Left:   // Left button pressed.
 			if (!running) timer.reset();
 			break;
 		default:
