a427e33ebeea31147736505f68ffc94997aa873d
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2021-10-15 18:21:51 -0600

    Add files via upload

--- examples/test_pwm/test_pwm.ino
+++ examples/test_pwm/test_pwm.ino
@@ -1,35 +1,40 @@
 #include <pg.h>
 #include <utilities/PWMOutput.h>
 using namespace pg;
+using Pwm = PWMOutput<>;
 
 // Program to demonstrate controlling LED brightness with PWM.
+// Note, not all Arduino boards support PWM output on pin 13.
 
-// Change this to the appropriate output pin for your board, if necessary.
+// Change this to an appropriate output pin for your board, if necessary.
+// Attaching an invalid pin will cause no output to be generated and the 
+// attach() method to return InvalidPin (0xFF).
 const pin_t pin = 13; 
+
 // Create the PWMOutput object attached to the given pin, with a duty cycle of 100% 
 // and enable PWM output (duty cycle range  = [0.0, 1.0] for 0% to 100%).
-Pwm pwm0(pin, 1.0, true); 
+Pwm pwm0(pin, 1.0f, true); 
 
 void setup() 
 {
   Serial.begin(9600);
-  // Turn off the LED.
-  digitalWrite(13, false); 
+  // Force output low.
+  digitalWrite(pin, false); 
   // Print the pwm params to serial monitor.
   Serial.println("");
   Serial.print("pin="); Serial.println(pwm0.attach());
   Serial.print("freq="); Serial.print(pwm0.frequency()); Serial.println(" Hz");
-  Serial.print("dc="); Serial.print(pwm0.duty_cycle() * 100.0); Serial.println("%");
+  Serial.print("dc="); Serial.print(pwm0.duty_cycle() * 100.0f); Serial.println("%");
   Serial.print("en="); Serial.println(pwm0.enabled());
 }
 
 void loop() 
 {
-  // Change the brightness every so often with the duty cycle parameter.
+  // Change the duty cycle every so often.
   delay(2000);
-  pwm0.duty_cycle(0.5);
+  pwm0.duty_cycle(0.5f);
   delay(2000);
-  pwm0.duty_cycle(0.0);
+  pwm0.duty_cycle(0.0f);
   delay(2000);
-  pwm0.duty_cycle(1.0);
+  pwm0.duty_cycle(1.0f);
 }
--- examples/test_serial/test_serial.ino
+++ examples/test_serial/test_serial.ino
@@ -4,19 +4,19 @@
 using namespace pg;
 using SerialController = SerialControl<8>;
 
-void f1() { Serial.println("f1"); }
-void f2() { Serial.println("f2"); }
+void f1() { Serial.println("f1() called"); }
+void f2() { Serial.println("f2() called"); }
 
 struct Cmd1:public icommand { void execute() override { f1(); } } cmd1;
 struct Cmd2:public icommand { void execute() override { f2(); } } cmd2;
 SerialController::value_type buf[32] = {'\0'};
 SerialController::Command commands[] = 
 {
-  SerialController::Command{{"cmd1",&cmd1}}, 
-  SerialController::Command{{"cmd2",&cmd2}}, 
+  SerialController::Command{{"cmd1",&cmd1}}, // Command string "cmd1" calls f1(). 
+  SerialController::Command{{"cmd2",&cmd2}}, // Command string "cmd2" calls f2(). 
 };
 
-SerialController sc(commands, true);
+SerialController sc(commands);
 
 void setup() 
 {
