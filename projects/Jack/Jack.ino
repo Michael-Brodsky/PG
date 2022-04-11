f77985c79760fb1cdaff735726393c86bc1c1698
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2022-04-11 00:14:10 -0600

    Add files via upload

--- projects/Jack/Jack.ino
+++ projects/Jack/Jack.ino
@@ -4,57 +4,93 @@
 #include <pg.h>
 #include <components/Jack.h>
 
+using pg::Connection;
+using pg::SerialConnection;
+using pg::UdpConnection;
 using pg::Jack;
 using pg::EEStream;
 using pg::RemoteControl;
 template<class... Ts>
 using RemoteCommand = RemoteControl::Command<void, void, Ts...>;
 
-void jackCallback();			// Client callback function, prints the current serial settings.
+void jackCallback();			// Client callback function, prints the current connection settings.
+void sendConnection(SerialConnection*, char*);
+void sendConnection(UdpConnection*, char*);
 void jackCommand(int);			// Remotely callable, user-defined function.
 void jackCommand(int, long);	// Remotely callable, user-defined function.
 void jackCommand(float);		// Remotely callable, user-defined function.
 
-serial<0> serial0;				// HardwareSerial object with extended capabilities (serial<0> references Serial).
+Connection* connect = nullptr;
 EEStream eeprom;				// EEPROM streaming object.
 RemoteCommand<int> cmd1("cmd1", &jackCommand);			// User-defined remote command calls jackCommand<int>.
 RemoteCommand<int, long> cmd2("cmd2", &jackCommand);	// User-defined remote command calls jackCommand<int, long>.
 RemoteCommand<float> cmd3("cmd3", &jackCommand);		// User-defined remote command calls jackCommand<float>.
-//Jack jack(serial0, eeprom, jackCallback, { &cmd1,&cmd2,&cmd3 });	// Jack object.
-Jack jack(serial0, eeprom, jackCallback);
+Jack jack(eeprom, & jackCallback, { &cmd1,&cmd2,&cmd3 });
 
 void setup() 
 {
-	jack.initialize();	// Retrieve settings from EEPROM & open Serial port.
-	jackCallback();		// Print the current Serial settings.
+	//connect = new SerialConnection(Serial);
+	//connect->open("56000,8N1");
+	connect = new UdpConnection();
+	connect->open("Drowning_Ophelia,Chinab13!!,5002");
+	if (connect->open())
+	{
+		connect->flush();
+		jack.connection(connect);
+		jack.initialize();	// This must be called any time the connection or command list changes.
+		digitalWrite(LED_BUILTIN, true);
+	}
 }
 
 void loop()
 {
-	jack.poll();		// Execute any new commands received by the serial port.
+	jack.poll();		// Execute any new commands received.
 }
 
 void jackCallback()
 {
-	char buf[17];
-	hardware_serial& comms = jack.comms();
-	auto it = std::find(hardware_serial::SupportedFrames.begin(), hardware_serial::SupportedFrames.end(), comms.frame());
+	char buf[32];
 
-	comms.printFmt(buf, "%lu,%s,%lu%c", comms.baud(), it->string(), comms.getTimeout(), (comms.isOpen()) ? '*' : ' ');
+	switch (jack.connection()->type())
+	{
+	case Connection::ConnectionType::Serial:
+		sendConnection(reinterpret_cast<SerialConnection*>(const_cast<Connection*>(jack.connection())), buf);
+		break;
+	case Connection::ConnectionType::Udp:
+		sendConnection(reinterpret_cast<UdpConnection*>(const_cast<Connection*>(jack.connection())), buf);
+		break;
+	default:
+		break;
+	}
+}
+ 
+void sendConnection(SerialConnection* connection, char* buf)
+{
+	std::sprintf(buf, "serial=%lu,%3s,%lu", connection->baud(), "8N1", connection->timeout());
+	connection->send(buf);
+}
+
+void sendConnection(UdpConnection* connection, char* buf)
+{
+	IPAddress ipa = connection->localIP();
+	std::sprintf(buf, "udp=%u.%u.%u.%u,%u", ipa[0],ipa[1],ipa[2],ipa[3], connection->port());
+	connection->send(buf);
 }
 
 void jackCommand(int i)
 {
 	char buf[64];
 
-	serial0.printFmt(buf, "jackCommand(%d)", i);
+	std::sprintf(buf, "jackCommand(%d)", i);
+	connect->send(buf);
 }
 
 void jackCommand(int i, long l)
 {
 	char buf[64];
 
-	serial0.printFmt(buf, "jackCommand(%d, %ld)", i, l);
+	std::sprintf(buf, "jackCommand(%d, %ld)", i, l);
+	connect->send(buf);
 }
 
 void jackCommand(float f)
@@ -65,5 +101,7 @@ void jackCommand(float f)
 
 	char buf[64];
 
-	serial0.printFmt(buf, "jackCommand(%.2f)", f);
+	std::sprintf(buf, "jackCommand(%.2f)", f);
+	connect->send(buf);
 }
+
